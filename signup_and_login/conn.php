c39fb8f80441fd894a9e41a185df714d39509dc4
ATHEER ALAHMADI <ahmdatherr@outlook.sa>
2023-12-04 03:14:22 +0300

    init

--- signup_and_login/conn.php
+++ signup_and_login/conn.php
@@ -1,6 +1,5 @@
 
 <?php 
-//test
 // conn variable to connect with the database
 // connect('the local host', 'in xampp it gives a default "root" username for the DB','the default password for the DB is empty', 'DB name')
 $conn = mysqli_connect('localhost', 'root', '', 'tshirtdesignmaker');

b27372d8eba7b30d45ee10ec85a576e5193c9d48
naella <naellaohun@gmail.com>
2023-12-05 05:19:52 +0300

    changing the shpiment DB error

--- shippmentTrack/conn.php
+++ shippmentTrack/conn.php
@@ -1,7 +1,7 @@
 <?php
 //connection to the database
 
-$conn = mysqli_connect('localhost','root','root','tshirtdesignmaker');
+$conn = mysqli_connect('localhost','root','','tshirtdesignmaker');
 
 if(!$conn){
 

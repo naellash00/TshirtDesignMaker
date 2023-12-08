c6255c9a57be56baa5c86842e854507002f3f775
ATHEER ALAHMADI <ahmdatherr@outlook.sa>
2023-12-09 02:16:50 +0300

    init

--- TshirtDesignMaker-2/payment-code/cart.php
+++ TshirtDesignMaker-2/payment-code/cart.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="author" content="Sahil Kumar">
+  <meta name="author" content="">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Cart</title>

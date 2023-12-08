0011b00d1fcb3c80542da15f3bee85b67c2ce215
RudinaWaslallah <rudinawasx@gmail.com>
2023-12-08 23:45:32 +0300

    init

--- payment-code/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// conn variable to connect with the database
-	$conn = new mysqli("localhost","root","","tshirtdesignmaker");
-	//Verify the connection
-	if($conn->connect_error){
-		die("Connection Failed!".$conn->connect_error);
-	}
-?>
